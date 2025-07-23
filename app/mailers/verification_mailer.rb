be113fce35c4f88de7cabedccc8519ea73818235
Aleks Rutins <keeper@farthergate.com>
2025-07-22 21:57:55 -0400

    use proper URL in verification email

--- app/mailers/verification_mailer.rb
+++ app/mailers/verification_mailer.rb
@@ -1,7 +1,10 @@
 class VerificationMailer < ApplicationMailer
   def verify(code)
     @user = User.find(code.user_id)
-    @verify_url = url_for("/verify?code=#{code.code}")
+    @verify_url = url_for(controller: "users",
+                          action: "verify",
+                          code: code.code,
+                          host: ENV["APP_HOST"] || ENV["RAILWAY_PUBLIC_DOMAIN"] || "localhost")
     mail subject: "Verify your account", to: @user.email_address
   end
 end
